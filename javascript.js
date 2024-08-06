c638d96dcdfa2671dc8750c584b459838d3f5c1a
Mylena <mylenadiovanna829@gmail.com>
2024-08-05 21:42:58 -0300

    Java

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var nome = prompt("digitei seu nome");
+
+        alert("Olá " + nome + "!!!");
+
+        var nome = "Mylena"; //texto -> string
+        var idade = 23; //number  -> integer
+        var peso = 34.6; // number -> float 
+        var acordado = true; // bloolean -> verdadeiro ou falso 
